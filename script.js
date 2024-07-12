7c4f2af15d5af7fb9e3ad63ab3910535f81e2ef4
Sujeevanan Loganathan <92037762+sujeevanan@users.noreply.github.com>
2024-07-12 12:18:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+//selct the input
+var output = document.getElementById("output");
+function display(num) {
+  output.value += num;
+}
+function calculate() {
+  output.value = eval(output.value);
+}
+function del() {
+  output.value = output.value.slice(0, -1);
+}
+function clr() {
+  output.value = "";
+}
